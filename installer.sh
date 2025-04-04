3bd0a17ae5664152ad29c91f23fb845eb000cea6
emil237 <marianatalaat1@gmail.com>
2025-04-04 19:53:58 +0200

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -2,69 +2,52 @@
 # ###########################################
 # SCRIPT : DOWNLOAD AND INSTALL ANSITE
 # ###########################################
-#
-# Command: wget https://raw.githubusercontent.com/emil237/ansite/main/installer.sh -qO - | /bin/sh
-#
-# ###########################################
 
 TMPDIR='/tmp'
 PACKAGE='enigma2-plugin-extensions-ansite'
 MY_URL='https://raw.githubusercontent.com/emil237/ansite/main'
 PYTHON_VERSION=$(python -c "import sys; print(sys.version_info.major)" 2>/dev/null || python -c "import sys; print(sys.version_info[0])")
 
-if [ -f /etc/opkg/opkg.conf ]; then
-    OSTYPE='Opensource'
+[ -f /etc/opkg/opkg.conf ] && {
     OPKG='opkg update'
-    OPKGINSTAL='opkg install'
+    OPKGINSTAL='opkg install --force-reinstall --force-depends'
     OPKGLIST='opkg list-installed'
-    OPKGREMOV='opkg remove --force-depends'
-fi
+    OPKGREMOV='opkg remove --force-depends --force-remove'
+}
 
-if [ "$PYTHON_VERSION" -eq 3 ]; then
-    echo "You have Python3 image..."
-    sleep 1
-    clear
-else
-    echo "You have Python2 image..."
-    sleep 1
-    clear
-fi
+echo "Detected Python$PYTHON_VERSION image..."
+sleep 1
+clear
 
 rm -rf "$TMPDIR/${PACKAGE:?}"* >/dev/null 2>&1
 
-INSTALLED_VERSION=$($OPKGLIST $PACKAGE 2>/dev/null | awk '{ print $3 }')
-
-if [ -n "$INSTALLED_VERSION" ]; then
+if INSTALLED_VERSION=$($OPKGLIST $PACKAGE 2>/dev/null | awk '{ print $3 }'); then
     if [ "$INSTALLED_VERSION" = "$VERSION" ]; then
-        echo "You are using the latest version: $VERSION"
-        exit 1
+        echo "Latest version $VERSION already installed"
+        exit 0
     else
-        echo "Removing previous version ($INSTALLED_VERSION)..."
-        $OPKGREMOV $PACKAGE
+        echo "Removing existing version ($INSTALLED_VERSION)..."
+        $OPKGREMOV $PACKAGE >/dev/null 2>&1
     fi
 fi
 
 echo "Updating package lists..."
 $OPKG >/dev/null 2>&1
 
-echo "Installing Ansite plugin. Please Wait..."
-if [ "$PYTHON_VERSION" -eq 3 ]; then
-    wget "$MY_URL/enigma2-plugin-extensions-ansite_1.10.py3_all.ipk" -qP "$TMPDIR"
-    $OPKGINSTAL "$TMPDIR/enigma2-plugin-extensions-ansite_1.10.py3_all.ipk"
-else
-    wget "$MY_URL/enigma2-plugin-extensions-ansite_1.5_py2_all.ipk" -qP "$TMPDIR"
-    $OPKGINSTAL "$TMPDIR/enigma2-plugin-extensions-ansite_1.5_py2_all.ipk"
-fi
+echo "Installing Ansite plugin..."
+PKG_FILE="enigma2-plugin-extensions-ansite_$(if [ "$PYTHON_VERSION" -eq 3 ]; then echo "1.10.py3_all.ipk"; else echo "1.5_py2_all.ipk"; fi)"
+wget "$MY_URL/$PKG_FILE" -qP "$TMPDIR" && $OPKGINSTAL "$TMPDIR/$PKG_FILE"
 
 rm -rf "$TMPDIR/${PACKAGE:?}"* >/dev/null 2>&1
 
-sleep 2
-echo ""
-echo "***********************************************************************"
-echo "**                                                                    *"      
-echo "**   >>>>>>>>>>  Uploaded by: EMIL_NABiL                     *"
-echo "**                       Developed by: aime_jeux                      *"
-echo "**                                                                    *"
-echo "***********************************************************************"
-echo ""
+cat <<EOF
+
+***********************************************************************
+**                                                                   **
+**   >>>>>>>>>>  Uploaded by: EMIL_NABiL                     **
+**                       Developed by: aime_jeux                      **
+**                                                                   **
+***********************************************************************
+
+EOF
 exit 0
